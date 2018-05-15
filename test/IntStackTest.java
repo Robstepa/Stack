53e8956484b752fa20ace89e9929912ba047f4aa
Robstepa <zb.arthur@gmail.com>
2018-05-15 11:12:00 +0200

    Create test for Stack

---
+++ test/IntStackTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntStackTest {
+
+    @Test
+    void testCreateInstance(){
+        Stack stack = new Stack();
+    }
+
+    @Test
+    void testCreateInstanceWithParameter(){
+        Stack stack = new Stack(15);
+    }
+
+    @Test
+    void testPushAndPeek(){
+        Stack<String> stack = new Stack<>();
+        String a = "a";
+        stack.push(a);
+        assertEquals("a", stack.peek());
+    }
+
+    @Test
+    void testPop(){
+        Stack<String> stack = new Stack<>();
+        String a = "a";
+        String b = "b";
+        stack.push(a);
+        stack.push(b);
+        assertEquals("b", stack.pop());
+        assertEquals("a", stack.pop());
+    }
+
+    @Test
+    void testPushToFullStack(){
+        Stack<String> stack = new Stack<>(2);
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        stack.push(a);
+        stack.push(b);
+        assertThrows(CustomStackException.class, ()->stack.push(c));
+    }
+
+    @Test
+    void testPeekFromEmptyStack(){
+        Stack<String> stack = new Stack<>();
+        assertThrows(CustomStackException.class, ()-> stack.peek());
+    }
+
+    @Test
+    void testPopFromEmptyStack(){
+        Stack<String> stack = new Stack<>();
+        assertThrows(CustomStackException.class, ()-> stack.pop());
+    }
+}
